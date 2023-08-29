89c6ea03ce827e86c5e7554c58c9d5d6b020622f
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-08-29 10:43:25 +0500

    Owl-fowl-branch merge

---
+++ calc.js
@@ -0,0 +1,21 @@
+export default class calc {
+  add(a, b) {
+    return a + b;
+  }
+  subtract(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    if (b===0) {
+      throw new Error("cannot divide by zero");
+    }
+    return a / b;
+  }
+}
+
+let features = branch.merge({
+  learn.git
+}
